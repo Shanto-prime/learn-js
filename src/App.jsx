c7c51ab9ee3bdbf23145ca799465e6ff2998ceeb
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-11-06 22:22:50 +0600

    refactor: remove unused SVG files and simplify App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [bgColor, setBgColor] = useState("#ffffff");
-
-  const handleClick = () => {
-    setTimeout(() => {
-      setBgColor((prevColor) => {
-        let color = prevColor.slice(1).split("");
-        const randomHex = () => Math.floor(Math.random() * 16).toString(16);
-        const indices = [0, 2, 5];
-        indices.forEach((i) => {
-          color[i] = randomHex();
-        });
-        return `#${color.join("")}`;
-      });
-    }, 300);
-  };
   return (
     <>
-      <div
-        onClick={handleClick}
-        style={{
-          backgroundColor: bgColor,
-          height: "100vh",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          transition: "background-color 0.5s ease",
-          cursor: "pointer",
-        }}
-      >
+      <div style={{ textAlign: "center" }}>
         <h1>Hello World</h1>
       </div>
     </>
