220cd95fe5d2fc53bd52fd80385e873dde4f7130
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-11-08 10:39:38 +0600

    feat: add Test component and update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Test from "./Components/Test";
+
 function App() {
   return (
     <>
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: "center", padding: "20px", background: "red" }}>
         <h1>Hello World</h1>
+        <Test />
       </div>
     </>
   );
