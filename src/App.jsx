1ea25033ca3963546c5dc6c0d6bc2cee27bab174
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-12-25 18:45:14 +0600

    feat: update App component to display statuses using Form component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Form from "./Components/Form";
 export default function App() {
   const statuses = ["Empty", "Submitting", "Success", "Error", "Typing"];
   return (
-    <div className="text-center flex justify-center m-10">
-      <h2>20+10-5-4-3-2-1</h2>
-      <p>=</p>
-      <Form />
+    <div className="text-center grid grid-cols-3 justify-center m-10">
+      {statuses.map((status) => (
+        <div className="m-2 flex justify-center">
+          <h2>20+10-5-4-3-2-1</h2>
+          <p>=</p>
+          <Form key={status} status={status} />
+        </div>
+      ))}
     </div>
   );
 }
