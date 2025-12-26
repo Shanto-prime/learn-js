81acd3bf70636ca51949d6df5ab7ad96ee62863b
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-12-27 01:35:39 +0600

    feat: refactor Form component to handle answer submission and display status messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
 import Form from "./Components/Form";
 export default function App() {
-  const statuses = ["Empty", "Submitting", "Success", "Error", "Typing"];
+  //   const statuses = ["Empty", "Submitting", "Success", "Error", "Typing"];
   return (
     <div className="text-center grid grid-cols-3 justify-center m-10">
-      {statuses.map((status) => (
-        <div className="m-2 flex justify-center">
-          <h2>20+10-5-4-3-2-1</h2>
-          <p>=</p>
-          <Form key={status} status={status} />
-        </div>
-      ))}
+      <div className="m-2 flex justify-center">
+        <h2>20+10-5-4-3-2-1</h2>
+        <p>=</p>
+        <Form />
+      </div>
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,31 +1,63 @@
-export default function Form({ status = "Empty" }) {
-  if (status === "Success") {
-    return <h1>Thats Right!</h1>;
-  }
+import { useState } from "react";
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer === "15") {
+        resolve();
+      } else {
+        reject(new Error("Wrong Answer vai"));
+      }
+    }, 1000);
+  });
+}
+
+export default function Form() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+
+  const handleTyping = (e) => {
+    setError(null);
+    setAnswer(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Submitting");
+    try {
+      await submitForm(answer);
+      setStatus("Success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err.message);
+    }
+  };
   return (
     <>
-      <form id="form">
+      <form onSubmit={handleSubmit}>
         <textarea
-          name="userInput"
-          id="input1"
+          value={answer}
+          onChange={handleTyping}
           rows="1"
-          disabled={status === "Submitting"}
           placeholder="?"
           className=" px-5"
+          disabled={status === "Submitting"}
         ></textarea>
         <br />
         <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
           type="submit"
-          disabled={status === "Submitting" || status === "Empty"}
+          disabled={status === "Submitting" || answer === ""}
         >
           Submit
         </button>
+        {status === "Success" && <p>Thats Right!</p>}
         {status === "Submitting" && <p>Loading...</p>}
-
-        {status === "Error" && (
-          <p className="error text-red-500">There Was An Error</p>
-        )}
+        {error && <p className="error text-red-500">{error}</p>}
+        {/* <p className="error text-red-500">There Was An Error</p> */}
       </form>
+      <hr />
     </>
   );
 }
