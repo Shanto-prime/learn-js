5eec2b548d52336931c4371a391ae439fcce5cd5
Shanto-prime <jahid.hasan.shanto@gmail.com>
2025-12-30 01:27:37 +0600

    feat: enhance Pointer component to manage ripple effects and optimize performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
+import Form from "./Components/Form";
 import Pointer from "./Components/Pointer";
 export default function App() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomNumber = Math.floor(Math.random() * 100);
+      console.log(`hello hi ${randomNumber}`);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
-      <Pointer />
-      {/* <div className="text-center grid grid-cols-3 justify-center m-10">
+      <div className="text-center grid grid-cols-3 justify-center m-10">
         <div className="m-2 flex justify-center">
           <h2>20+10-5-4-3-2-1</h2>
           <p>=</p>
           <Form />
         </div>
-      </div> */}
+      </div>
+      <Pointer />
     </>
   );
 }
--- src/Components/Pointer.jsx
+++ src/Components/Pointer.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Pointer() {
   const [ripples, setRipples] = useState([]);
 
-  const handlePointerMove = (e) => {
-    const ripple = {
-      id: crypto.randomUUID(),
-      x: e.clientX,
-      y: e.clientY,
+  useEffect(() => {
+    const handleMove = (e) => {
+      const ripple = {
+        id: crypto.randomUUID(),
+        x: e.clientX,
+        y: e.clientY,
+      };
+
+      setRipples((prev) => [...prev.slice(-15), ripple]);
+
+      setTimeout(() => {
+        setRipples((prev) => prev.filter((r) => r.id !== ripple.id));
+      }, 700);
     };
 
-    setRipples((prev) => [...prev, ripple]);
-
-    // remove after animation
-    setTimeout(() => {
-      setRipples((prev) => prev.filter((r) => r.id !== ripple.id));
-    }, 700);
-  };
+    window.addEventListener("pointermove", handleMove);
+    return () => window.removeEventListener("pointermove", handleMove);
+  }, []);
 
   return (
-    <div
-      className="relative h-dvh w-dvw overflow-hidden"
-      onPointerMove={handlePointerMove}
-    >
+    <div className="fixed inset-0 z-50 pointer-events-none">
       {ripples.map((ripple) => (
         <div
           key={ripple.id}
-          className="pointer-events-none absolute w-3 h-3 rounded-full cursor-ripple"
+          className="absolute w-3 h-3 rounded-full cursor-ripple"
           style={{
             left: ripple.x,
             top: ripple.y,
