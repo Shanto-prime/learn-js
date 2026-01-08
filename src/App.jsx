a35707bb1b70c556ad98a26769c00e93d29473d5
Shanto-prime <jahid.hasan.shanto@gmail.com>
2026-01-08 20:25:43 +0600

    feat: remove unused useEffect for random number logging in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import Pointer from "./Components/Pointer";
 import TravelPlan from "./Components/TravelPlan";
 export default function App() {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const randomNumber = Math.floor(Math.random() * 100);
-      console.log(`hello hi ${randomNumber}`);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
   return (
     <>
       <TravelPlan />
