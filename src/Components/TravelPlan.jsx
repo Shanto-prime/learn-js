689ac1d89c897f065d61ff6b530850e4d0ee701c
Shanto-prime <jahid.hasan.shanto@gmail.com>
2026-01-11 23:29:17 +0600

    feat: add delete functionality to TravelPlan and TravelTree components

--- src/Components/TravelPlan.jsx
+++ src/Components/TravelPlan.jsx
@@ -5,12 +5,30 @@ export default function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan);
   const root = plan[0];
   const PlanetIds = root.childIds;
+  const handleDelete = (parentId, selfId) => {
+    const parent = plan[parentId];
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter((id) => id !== selfId),
+    };
+    setPlan({
+      ...plan,
+      nextParent,
+    });
+    console.log("delete btn pressed");
+  };
   return (
     <>
       <h5>TravelPlan</h5>
       <ol className="pl-4 list-decimal list-inside">
         {PlanetIds.map((id) => (
-          <TravelTree key={id} placesId={plan} id={id} />
+          <TravelTree
+            key={id}
+            placesId={plan}
+            id={id}
+            onClick={handleDelete}
+            parentId={0}
+          />
         ))}
       </ol>
     </>
--- src/Components/TravelTree.jsx
+++ src/Components/TravelTree.jsx
@@ -1,14 +1,26 @@
-export default function TravelTree({ placesId, id }) {
+export default function TravelTree({ placesId, id, onClick, parentId }) {
   const place = placesId[id];
   const childIds = place.childIds;
   return (
     <>
       <li>
         {place.title}
+        <button
+          className="text-red-500 border-2 border-red-500 rounded-2xl px-2 mx-3"
+          onClick={() => onClick(id, parentId)}
+        >
+          Delete
+        </button>
         <ol className="pl-4 list-decimal list-inside">
           {childIds.length > 0 &&
             childIds.map((id) => (
-              <TravelTree key={id} id={id} placesId={placesId} />
+              <TravelTree
+                key={id}
+                id={id}
+                parentId={parentId}
+                placesId={placesId}
+                onClick={onClick}
+              />
             ))}
         </ol>
       </li>
