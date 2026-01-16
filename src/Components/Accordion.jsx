fd72614f23fd25efe630ed85b17c948d3dd8d158
Shanto-prime <jahid.hasan.shanto@gmail.com>
2026-01-16 23:07:43 +0600

    feat: enhance Accordion and Panel components with active state management

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Panel from "./panel";
 
 export default function Accordion() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    // const handleActiveIndex = (index) => setActiveIndex(index);
     return (
         <>
-            <Panel title="Lite version">
+            <Panel
+                title="Lite version"
+                isActive={activeIndex === 0 && true}
+                onActiveHandler={() => setActiveIndex(0)}
+            >
                 dolor sit amet consectetur adipisicing elit. Praesentium,
                 voluptate.
             </Panel>
-            <Panel title="Paid version">
+            <Panel
+                title="Paid version"
+                isActive={activeIndex === 1 && true}
+                onActiveHandler={() => setActiveIndex(1)}
+            >
                 dolor sit, amet consectetur adipisicing elit. Consequatur harum
                 officiis nam porro cumque exercitationem?
             </Panel>
--- src/Components/Panel.jsx
+++ src/Components/Panel.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-export default function Panel({ title, children }) {
-    const [isShow, setIsShow] = useState(false);
-    const handleShow = () => setIsShow(!isShow);
+export default function Panel({ title, children, isActive, onActiveHandler }) {
+    // const handleShow = () => setIsShow(!isShow);
     return (
         <>
             <div className="p-2 m-2 border-amber-500 border-2">
                 <h3>{title}</h3>
-                {isShow ? (
+                {isActive ? (
                     <p>{children}</p>
                 ) : (
                     <button
-                        onClick={handleShow}
+                        onClick={onActiveHandler}
                         className="text-red-500 border-2 border-red-500 rounded-2xl px-2 mx-3"
                     >
                         Show
