28e4c7f7577fbcee59f755c554de6815589ee12b
Shanto-prime <jahid.hasan.shanto@gmail.com>
2026-01-26 22:06:10 +0600

    feat: add TravelPlan component to App for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Pointer from "./Components/Pointer";
 import Test from "./Components/Test";
+import TravelPlan from "./Components/TravelPlan";
 export default function App() {
     return (
         <>
             <Test />
+            <TravelPlan />
             <Pointer />
         </>
     );
