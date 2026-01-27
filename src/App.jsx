a2ac7e1b6d9bf58647d8deac1469959e443dc5fb
Shanto-prime <jahid.hasan.shanto@gmail.com>
2026-01-27 23:10:20 +0600

    refactor: remove unused Accordion and TravelPlan components from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Accordion from "./Components/Accordion";
 import Pointer from "./Components/Pointer";
 import Test from "./Components/Test";
-import TravelPlan from "./Components/TravelPlan";
 export default function App() {
     return (
         <>
             <Test />
-            <TravelPlan />
-            <Accordion />
             <Pointer />
         </>
     );
